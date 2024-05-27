036aac4be6e3331af7f64e3791e37896bd0a7914
Adibkhan619 <work.adibkhan@gmail.com>
2024-05-27 15:55:43 +0600

    Added payment intent

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
